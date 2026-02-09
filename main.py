ab93ecd18f9182de1e183170d1fd9554bdcdc3da
ayimany <ayimany.dev@proton.me>
2026-02-08 20:51:33 -0600

    Create a primitive but working pipeline

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import argparse
+import subprocess
+
 def main():
-    pass
+    command = ['prusa-slicer']
+
+    parser = argparse.ArgumentParser(
+        prog='scratchpole',
+        description='Helps you scratch my pole',
+        epilog='Happy slicing.',
+    )
+
+    parser.add_argument('file', metavar='FILE', type=str)
+    parser.add_argument('--settings', metavar='FILE', type=str, nargs='*', required=True)
+
+    args = parser.parse_args()
+
+    for setting in args.settings:
+        command.append('--load')
+        command.append(setting)
+
+    command.append('-g')
+    command.append('-s')
+    command.append(args.file)
+
+    print(command)
+
+    subprocess.run(command)
+
 
 if __name__ == '__main__':
     main()
